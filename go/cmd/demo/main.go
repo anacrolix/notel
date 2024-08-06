3a10265dc612d962337029d97b763b1dcf44f758
Matt Joiner <anacrolix@gmail.com>
2024-08-06 15:52:16 +1000

    Add -url flag to writer demo

--- go/cmd/demo/main.go
+++ go/cmd/demo/main.go
@@ -5,22 +5,39 @@ import (
 	telemetry "github.com/anacrolix/telemetry/go"
 	"log/slog"
 	"net/url"
+	"os"
 )
 
 func main() {
+	err := mainErr()
+	if err != nil {
+		slog.Error("error in main", "err", err)
+		os.Exit(1)
+	}
+}
+
+func mainErr() (err error) {
 	// Can switch to http for example to demonstrate POST.
 	scheme := flag.String("scheme", "ws", "telemetry scheme")
+	urlStr := flag.String("url", "", "telemetry url")
 	flag.Parse()
+	_url := &url.URL{
+		Scheme: *scheme,
+		// Should derive this from the default port somewhere.
+		Host: "localhost:4318",
+	}
+	if *urlStr == "" {
+		_url, err = url.Parse(*urlStr)
+		if err != nil {
+			return err
+		}
+	}
 	telemetryWriter := telemetry.Writer{
-		Url: &url.URL{
-			Scheme: *scheme,
-			// Should derive this from the default port somewhere.
-			Host: "localhost:4318",
-		},
+		Url: _url,
 	}
 	jsonHandler := slog.NewJSONHandler(&telemetryWriter, nil)
 	logger := slog.New(jsonHandler)
 	logger.Info("hi mom")
 	// TODO: This message could be written to the streams table.
-	telemetryWriter.Close("i have to go now. goodbye!")
+	return telemetryWriter.Close("i have to go now. goodbye!")
 }
