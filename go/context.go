700ae570b293e6c8ab6d00cea41c50b8b830c699
Matt Joiner <anacrolix@gmail.com>
2024-08-15 23:34:44 +1000

    go: Add context Slogger

--- go/context.go
+++ go/context.go
@@ -1,1 +1,25 @@
 package telemetry
+
+import (
+	"context"
+	"log/slog"
+)
+
+type sloggerKey struct{}
+
+// Adds the slog.Logger into the context. Presumably the slog.Logger is used for telemetry.
+func ContextWithSlogger(ctx context.Context, l *slog.Logger) context.Context {
+	return context.WithValue(ctx, sloggerKey{}, l)
+}
+
+func SloggerFromContext(ctx context.Context) *slog.Logger {
+	return ctx.Value(sloggerKey{}).(*slog.Logger)
+}
+
+func MustSloggerFromContext(ctx context.Context) *slog.Logger {
+	slogger := SloggerFromContext(ctx)
+	if slogger == nil {
+		panic("where banana")
+	}
+	return slogger
+}
